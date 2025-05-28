0308acdd9a95f0c3d668b49cb3feec15aa897981
zain1234-lab <mohsinchauhan1647@gmail.com>
2025-05-28 22:37:54 +0500

    Delete static.js

--- static.js
+++
@@ -1,9 +0,0 @@
-function saveDraft(formData, callback) {
-    fetch('/draft/create', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => callback(data))
-    .catch(error => callback({ success: false, message: error.message }));
-}
